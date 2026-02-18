7280c09690a2a53f24ca53e7caad4f93d307b9f5
RijuChoudhary <rijuchoudhary15@gmail.com>
2026-02-18 18:14:41 +0530

    Refactor BaseClass for improved browser handling
    Updated import statements and modified the order of operations in the setup method.

--- src/test/java/utility/BaseClass.java
+++ src/test/java/utility/BaseClass.java
@@ -1,5 +1,6 @@
 package utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -8,6 +9,8 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+
+import utility.ListenersLogic;
 
 public class BaseClass extends ListenersLogic {
 	
@@ -33,9 +36,10 @@ public class BaseClass extends ListenersLogic {
 			
 		}
        driver.get("https://www.amazon.in/");
-       driver.manage().window().maximize();
-		Thread.sleep(3000);
-       driver.findElement(By.xpath("//button[text()='Continue shopping']")).click();
+       Thread.sleep(3000);
+    driver.findElement(By.xpath("//button[text()='Continue shopping']")).click();
+       
+       
 		
 	}
 	
